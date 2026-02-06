91c57d167aa346b2ee2699d15f113dd754ce2dbd
Vitor Pavani <vitoralvespavani@gmail.com>
2026-02-06 14:14:03 -0300

    [Fix]: Update lsp.lua

--- nvim/.config/nvim/lua/user/plugins/lsp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp.lua
@@ -1,78 +1,153 @@
 local M = {
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = {
-        'neovim/nvim-lspconfig',
-        'saghen/blink.cmp',
-    },
-    config = function()
-     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
-      })
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "saghen/blink.cmp",
+    },
+
+    -- Optional: auto-install these if missing
+    opts = {
+      ensure_installed = { "lua_ls", "html" },
+      -- automatic_enable = true, -- default in v2
+    },
+
+    config = function(_, opts)
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      -- Neovim 0.11+ (mason-lspconfig v2 expects this)
+      if vim.lsp and vim.lsp.config and vim.lsp.enable then
+        -- Apply capabilities to all servers by default
+        vim.lsp.config("*", {
+          capabilities = capabilities,
+        })
+
+        -- gdscript (usually not managed by Mason; enable manually)
+        vim.lsp.config("gdscript", {})
+
+        -- HTML (add templ filetype + your settings)
+        vim.lsp.config("html", {
+          filetypes = { "html", "templ" },
+          settings = {
+            html = {
+              format = {
+                templating = true,
+                wrapLineLength = 120,
+                wrapAttributes = "auto",
+              },
+              hover = {
+                documentation = true,
+                references = true,
+              },
+            },
+          },
+        })
+
+        -- lua_ls
+        local runtime_path = vim.split(package.path, ";")
+        table.insert(runtime_path, "lua/?.lua")
+        table.insert(runtime_path, "lua/?/init.lua")
+
+        vim.lsp.config("lua_ls", {
+          settings = {
+            Lua = {
+              runtime = {
+                version = "LuaJIT",
+                path = runtime_path,
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              telemetry = {
+                enable = false,
+              },
+            },
+          },
+        })
+
+        -- Set up mason-lspconfig (auto-enables Mason-installed servers by default)
+        require("mason-lspconfig").setup(opts)
+
+        -- Enable servers NOT installed via Mason (gdscript typically)
+        vim.lsp.enable("gdscript")
+
+        return
+      end
+
+      ----------------------------------------------------------------------
+      -- Fallback: older Neovim / older flow (keeps you from crashing)
+      ----------------------------------------------------------------------
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup(opts)
 
       local lspconfig = require("lspconfig")
 
-      -- Specific LSP configurations
-      lspconfig.gdscript.setup {}
+      local function setup(server, server_opts)
+        server_opts = server_opts or {}
+        server_opts.capabilities = capabilities
+        lspconfig[server].setup(server_opts)
+      end
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
+      -- Generic setup for Mason-installed servers (skip ones we configure explicitly)
+      local special = { lua_ls = true, html = true, gdscript = true }
+      for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+        if not special[server] and lspconfig[server] then
+          setup(server, {})
+        end
+      end
+
+      -- Explicit servers
+      setup("gdscript", {})
+      setup("html", {
         filetypes = { "html", "templ" },
         settings = {
           html = {
             format = {
               templating = true,
               wrapLineLength = 120,
-              wrapAttributes = 'auto',
+              wrapAttributes = "auto",
             },
             hover = {
               documentation = true,
               references = true,
             },
           },
-        }
+        },
       })
-
-      -- Lua LSP configuration
-      local runtime_path = vim.split(package.path, ";")
-      table.insert(runtime_path, "lua/?.lua")
-      table.insert(runtime_path, "lua/?/init.lua")
-
-      lspconfig.lua_ls.setup({
+      setup("lua_ls", {
         settings = {
           Lua = {
             runtime = {
               version = "LuaJIT",
-              path = runtime_path,
+              path = (function()
+                local rp = vim.split(package.path, ";")
+                table.insert(rp, "lua/?.lua")
+                table.insert(rp, "lua/?/init.lua")
+                return rp
+              end)(),
             },
-            diagnostics = {
-              globals = { "vim" },
-            },
+            diagnostics = { globals = { "vim" } },
             workspace = {
               library = vim.api.nvim_get_runtime_file("", true),
               checkThirdParty = false,
             },
-            telemetry = {
-              enable = false,
-            },
+            telemetry = { enable = false },
           },
-        }
+        },
       })
-    end
-  }
+    end,
+  },
 }
 
 return { M }
